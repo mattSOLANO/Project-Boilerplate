5132df381db028e6746f7bdee694e92216198051
Paul Demers <paul@demersdesigns.com>
2013-08-05 12:51:43 -0400

    Concatenation of head JS

---
+++ js/vendor/matchmedia.js
@@ -0,0 +1,3 @@
+/*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
+/*! NOTE: If you're already including a window.matchMedia polyfill via Modernizr or otherwise, you don't need this part */
+window.matchMedia=window.matchMedia||(function(e,f){var c,a=e.documentElement,b=a.firstElementChild||a.firstChild,d=e.createElement("body"),g=e.createElement("div");g.id="mq-test-1";g.style.cssText="position:absolute;top:-100em";d.style.background="none";d.appendChild(g);return function(h){g.innerHTML='&shy;<style media="'+h+'"> #mq-test-1 { width: 42px; }</style>';a.insertBefore(d,b);c=g.offsetWidth==42;a.removeChild(d);return{matches:c,media:h}}})(document);
---
+++ js/vendor/vendor.js
@@ -0,0 +1,3 @@
+// @codekit-append "modernizr-2.6.2.min.js"
+// @codekit-append "respond-1.1.0.min.js"
+// @codekit-append "matchmedia.js"
